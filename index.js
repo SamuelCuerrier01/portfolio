28be9c38b28b1dc3fcc2b67bb2eeb118bb25b7a0
Samuel Cuerrier <6297553@cstjean.qc.ca>
2025-09-17 14:35:49 -0400

    commit desdeux fonctions qui ne marche pas encore

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('Happy developing ✨')
+
+let body = document.getElementsByTagName('body');
+
+body.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+})
+
+body.addEventListener('dblclick', (e) => {
+    e.preventDefault()
+})
+
+//les deux tests ne mache pas
